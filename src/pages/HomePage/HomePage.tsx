ae5436d4f2a4b2ca98b4da7b8473f27f67534880
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-01-31 20:24:42 +0330

    Add Name To Home Page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <div>
+
+            <Typography align='center' fontSize={72} gutterBottom>Amir Mahdi Keshan Zare</Typography>
+
+            <Typography align='center' fontSize={45}>Front End Developer</Typography>
+        </div>
+    )
+}
--- src/route/AppRouter.tsx
+++ src/route/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "components/Layout/Layout";
+import { HomePage } from "pages";
 
 const AppRouter = () => {
     return (
         <Layout >
             <BrowserRouter>
                 <Routes>
-                    <Route index element={'HomePage'} />
+                    <Route index element={<HomePage />} />
                 </Routes>
             </BrowserRouter>
         </Layout>
