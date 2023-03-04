e98e99e09cc78f2de5df7b0f0d658b168261dce8
amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-04 23:52:14 +0330

    Modify Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, { ReactNode } from 'react'
+import { Box } from '@mui/system'
 import { Header } from './Header/Header'
 
 export const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <>
             <Header />
+            <Box mt={{ xs: 8, lg: 4 }}></Box>
             {children}
         </>
     )
