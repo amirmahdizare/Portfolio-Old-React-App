ca80e10da293154e7de2d6b5a8805d5aba3a6c2c
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-26 18:32:55 +0430

    Add Container to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
+import { Container } from '@mui/material'
 import React, { ReactNode } from 'react'
 import { Header } from './Header/Header'
 
 export const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <>
+        <Container maxWidth='xl' >
             <Header />
             {children}
-        </>
+        </Container>
     )
 }
