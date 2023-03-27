0a58ea51dac6a76c67dabf0d6587ec68623995d0
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-28 00:05:09 +0430

    Change Hero SectionColor

--- src/pages/HomePage/components/HeroSection/HeroSection.tsx
+++ src/pages/HomePage/components/HeroSection/HeroSection.tsx
@@ -4,8 +4,8 @@ import { Typography } from '@mui/material'
 
 export const HeroSection = () => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: "100%", justifyContent: 'center' }}>
-            <Box sx={{ background: '#1a1a1a', display: 'flex', flexDirection: 'column', alignItems: 'center', width: "100%",boxSizing:'border-box', justifyContent: 'center', p: 3 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: "100%", justifyContent: 'center' , background:'ra dial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)'}}>
+            <Box sx={{ background: '#161616', display: 'flex', flexDirection: 'column', alignItems: 'center', width: "100%",boxSizing:'border-box', justifyContent: 'center', p: 3 }}>
 
                 <Typography fontSize={24} color={'white'} gutterBottom>Are you looking for a experinced front end developer ?</Typography>
                 <Typography fontSize={24} color={'white'} gutterBottom>Do you need front end applications to achieve your business goals?</Typography>
